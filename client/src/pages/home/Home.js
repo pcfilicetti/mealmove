a5e70413fedcb986c72df2ecf051840a4da2a90f
em <6em.rva@gmail.com>
2018-10-30 11:46:53 -0400

    start on front end stuff and grommet

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,89 @@
+// import Article from 'grommet/components/Article';
+// import Columns from 'grommet/components/Columns';
+import Box from 'grommet/components/Box';
+import Sidebar from 'grommet/components/Sidebar';
+// import Section from 'grommet/components/Section';
+import Hero from 'grommet/components/Hero';
+// import Form from 'grommet/components/Form';
+// import FormField from 'grommet/components/FormField';
+// import LoginForm from 'grommet/components/LoginForm';
+// import Select from 'grommet/components/Select';
+// import RadioButton from 'grommet/components/RadioButton';
+// import Button from 'grommet/components/Button';
+import Header from 'grommet/components/Header';
+import Footer from 'grommet/components/Footer';
+// import AnnotatedMeter from 'grommet-addons/components/AnnotatedMeter';
+import App from 'grommet/components/App';
+import Title from 'grommet/components/Title';
+import Menu from 'grommet/components/Menu';
+import Anchor from 'grommet/components/Anchor';
+import Image from 'grommet/components/Image';
+import Heading from 'grommet/components/Heading';
+
+
+
+import React, { Component } from "react";
+// import API from "../../utils/API";
+
+class Replate extends Component {
+    
+    state = {};
+
+
+    render () {
+        return (
+            <App>
+            <Sidebar colorIndex='neutral-1'>
+                <Header pad='medium'
+                    justify='between'>
+                    <Title>
+                    Replate
+                    </Title>
+                </Header>
+                <Box flex='grow'
+                    justify='start'>
+                    <Menu primary={true}>
+                    <Anchor href='#'
+                        className='active'>
+                        First
+                    </Anchor>
+                    <Anchor href='#'>
+                        Second
+                    </Anchor>
+                    <Anchor href='#'>
+                        Third
+                    </Anchor>
+                    </Menu>
+                </Box>
+                <Footer pad='medium'>
+                    {/* <Button icon={} /> */}
+                </Footer>
+            </Sidebar>
+
+            <Hero background={<Image src="https://images.pexels.com/photos/349610/pexels-photo-349610.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                fit='cover'
+                full={true} />}
+                backgroundColorIndex='dark'
+                size="large">
+                {/* <Box direction='row'
+                    justify='center'
+                    align='center'>
+                <Box basis='1/2'
+                    align='end'
+                    pad='medium' />
+                <Box basis='1/2'
+                    align='start'
+                    pad='medium'>
+                <Heading margin='none'>
+                        Replate
+                </Heading>
+                </Box>
+                </Box> */}
+            </Hero>
+            </App>
+        );
+    }
+
+}
+
+export default Replate;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
