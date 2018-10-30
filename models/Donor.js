bcd8f38469df60c675dc31c45b9e9e9926139f96
Keiyon Tyler <tookie@Keiyons-MacBook-Pro.local>
2018-10-30 11:48:41 -0400

    filled in info for models and controller folder

---
+++ models/Donor.js
@@ -0,0 +1,15 @@
+//date they can pick up
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const donorSchema = new Schema({
+    companyName: { type: String, required: true },
+    address: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    availableDate: { $dateToString: { format: "%Y-%m-%d", date: "$date"} },
+});
+
+const Donor = mongoose.model("Donor", donorSchema);
+
+module.exports = Donor;
---
+++ models/Volunteer.js
@@ -0,0 +1,16 @@
+// date they are available
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const volunteerSchema = new Schema({
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    email: { type: String, required: true },
+    availableDate: { $dateToString: { format: "%Y-%m-%d", date: "$date"} },
+});
+
+const Volunteer = mongoose.model("Volunteer", volunteerSchema);
+
+module.exports = Volunteer;
