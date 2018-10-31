8809c6736882b6a224bfe872649ef373a50910a3
PCFilicetti <peter.filicetti@kingsmantech.com>
2018-10-31 15:09:35 -0400

    commented out user auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import Replate from './pages/home'
-import { Security, ImplicitCallback } from '@okta/okta-react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Home';
+// import { Security, ImplicitCallback } from '@okta/okta-react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import Home from './Home';
 
 
-const config = {
-  issuer: 'https://dev-606373.oktapreview.com/oauth2/default',
-  redirect_uri: window.location.origin + '/implicit/callback',
-  client_id: '{clientId}'
-}
+// const config = {
+//   issuer: 'https://dev-606373.oktapreview.com/oauth2/default',
+//   redirect_uri: window.location.origin + '/implicit/callback',
+//   client_id: ''
+// }
 
 
 class App extends Component {
   render() {
     return (
-
-<div>
-      
-
+      <div>
       <Replate />
-      <Router>
+      {/* <Router>
         <Security issuer={config.issuer}
                   client_id={config.client_id}
                   redirect_uri={config.redirect_uri}
@@ -28,12 +25,8 @@ class App extends Component {
           <Route path='/' exact={true} component={Home}/>
           <Route path='/implicit/callback' component={ImplicitCallback}/>
         </Security>
-      </Router>
-
-</div>
-
-      
-
+      </Router> */}
+      </div>
     );
   }
 }
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,41 +1,41 @@
-import React, { Component } from 'react';
-import { withAuth } from '@okta/okta-react';
+// import React, { Component } from 'react';
+// import { withAuth } from '@okta/okta-react';
 
-export default withAuth(class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { authenticated: null };
-    this.checkAuthentication = this.checkAuthentication.bind(this);
-    this.checkAuthentication();
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-  }
+// export default withAuth(class Home extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { authenticated: null };
+//     this.checkAuthentication = this.checkAuthentication.bind(this);
+//     this.checkAuthentication();
+//     this.login = this.login.bind(this);
+//     this.logout = this.logout.bind(this);
+//   }
 
-  async checkAuthentication() {
-    const authenticated = await this.props.auth.isAuthenticated();
-    if (authenticated !== this.state.authenticated) {
-      this.setState({ authenticated });
-    }
-  }
+//   async checkAuthentication() {
+//     const authenticated = await this.props.auth.isAuthenticated();
+//     if (authenticated !== this.state.authenticated) {
+//       this.setState({ authenticated });
+//     }
+//   }
 
-  componentDidUpdate() {
-    this.checkAuthentication();
-  }
+//   componentDidUpdate() {
+//     this.checkAuthentication();
+//   }
 
-  async login() {
-    // Redirect to '/' after login
-    this.props.login('/');
-  }
+//   async login() {
+//     // Redirect to '/' after login
+//     this.props.login('/');
+//   }
 
-  async logout() {
-    // Redirect to '/' after logout
-    this.props.logout('/');
-  }
+//   async logout() {
+//     // Redirect to '/' after logout
+//     this.props.logout('/');
+//   }
 
-  render() {
-    if (this.state.authenticated === null) return null;
-    return this.state.authenticated ?
-      <button onClick={this.logout}>Logout</button> :
-      <button onClick={this.login}>Login</button>;
-  }
-});
+//   render() {
+//     if (this.state.authenticated === null) return null;
+//     return this.state.authenticated ?
+//       <button onClick={this.logout}>Logout</button> :
+//       <button onClick={this.login}>Login</button>;
+//   }
+// });
