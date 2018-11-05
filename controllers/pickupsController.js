2c51c29c01e58267d05d4e6dc075f2c3bd002f6f
PCFilicetti <peter.filicetti@kingsmantech.com>
2018-11-05 11:04:47 -0500

    removed index.js in models

--- controllers/pickupsController.js
+++ controllers/pickupsController.js
@@ -1,30 +1,30 @@
-const db = require("../models");
+const db = require("../models/Pickup");
 
 module.exports = {
 
     findAll: function(req, res) {
-        db.Pickup
+        db
         .find(req.query)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
 
     findById: function(req, res) {
-        db.Pickup
+        db
         .findById(req.params.id)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
 
     create: function(req, res) {
-        db.Pickup
+        db
         .create(req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
 
     update: function(req, res) {
-        db.Pickup
+        db
         .findOneAndUpdate({ _id: req.params.id }, req.body)
         .then(dbModel => req.json(dbModel))
         .catch(err => res.status(422).json(err));
--- models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    Pickup: require("./Pickup")
-  };
